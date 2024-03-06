37908ac6806fdfc34e7810db7a878f06ac2bf3c9
Murica <160842487+freedomOilHunter@users.noreply.github.com>
2024-03-06 14:09:08 +0000

    eat

--- bob.py
+++ bob.py
@@ -1,9 +1,9 @@
-body=f"Hi, {name}, thank you for shopping with Cafe George. Enjoy your order!" # your message body goes here
+import smtplib
+body=f"eat" # your message body goes here
 subject="Your order from Cafe George" # your subject goes here
-recipients=[email] # list of your recipients
+recipients=["21gbower@kes.net"] # list of your recipients
 smtpObj = smtplib.SMTP('smtp.office365.com', 587) # object init
 smtpObj.ehlo() # handshake
 smtpObj.starttls() # encryption
-smtpObj.login('cafe_george@outlook.com', "Messaging1") # also outlook login details
-smtpObj.sendmail('cafe_george@outlook.com', recipients, f"Subject: {subject}\n\n{body}") # create & send the email
+smtpObj.sendmail('21rhall@kes.net', recipients, f"Subject: {subject}\n\n{body}") # create & send the email
 smtpObj.quit() # terminate session
