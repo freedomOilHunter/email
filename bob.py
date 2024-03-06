61d3882b0edbcf4c4720acf8c30b609990d4491b
Murica <160842487+freedomOilHunter@users.noreply.github.com>
2024-03-06 14:14:22 +0000

    fr

--- bob.py
+++ bob.py
@@ -1,9 +1,23 @@
-import smtplib
-body=f"eat" # your message body goes here
-subject="Your order from Cafe George" # your subject goes here
-recipients=["21gbower@kes.net"] # list of your recipients
-smtpObj = smtplib.SMTP('smtp.office365.com', 587) # object init
-smtpObj.ehlo() # handshake
-smtpObj.starttls() # encryption
-smtpObj.sendmail('21rhall@kes.net', recipients, f"Subject: {subject}\n\n{body}") # create & send the email
-smtpObj.quit() # terminate session
+import smtplib, ssl
+
+smtp_server = "smtp.gmail.com"
+port = 587  # For starttls
+sender_email = "my@gmail.com"
+password = input("Type your password and press enter: ")
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+# Try to log in to server and send email
+try:
+    server = smtplib.SMTP(smtp_server,port)
+    server.ehlo() # Can be omitted
+    server.starttls(context=context) # Secure the connection
+    server.ehlo() # Can be omitted
+    server.login(sender_email, password)
+    # TODO: Send email here
+except Exception as e:
+    # Print any error messages to stdout
+    print(e)
+finally:
+    server.quit()
